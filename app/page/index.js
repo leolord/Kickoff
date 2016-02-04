6abd86125c7efb1bc1686533a889d84c247ca8ba
wujiantao <wujiantao@5tv.com>
2016-02-04 15:17:33 +0800

    删除无用的依赖

---
+++ app/page/index.js
@@ -0,0 +1,13 @@
+/*
+ * file: ./app/page/index.js
+ * author: 吴建涛<fortomorrow@163.com>
+ * description: page
+ */
+
+'use strict';
+
+require('./index.scss');
+
+let msg = require('./msg.json').msg;
+
+console.log(msg);
